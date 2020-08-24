df18c7fc41c0f659bd3cc3a827aaa2abca78f45f
asokraju <kkrishnachaitanya89@gmail.com>
2020-08-23 22:06:50 -0400

    Changed parameter
    more realistic

--- microgrid_dcbf.py
+++ microgrid_dcbf.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
     parser.add_argument('--state_dim', help='state dimension of environment', type = int, default=state_dim)
     parser.add_argument('--action_dim', help='action space dimension', type = int, default=action_dim)
     parser.add_argument('--action_bound', help='upper and lower bound of the actions', type = float, default=action_bound)
-    parser.add_argument('--discretization_time', help='discretization time used for the environment ', type = float, default=1e-3)
+    parser.add_argument('--discretization_time', help='discretization time used for the environment ', type = float, default=1e-5)
 
     #Network parameters
     parser.add_argument('--time_steps', help='Number of time-steps for rnn (LSTM)', type = int, default=2)
