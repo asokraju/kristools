92de74c222da46666c17becff9dec3305924c45c
asokraju <kkrishnachaitanya89@gmail.com>
2020-08-25 03:42:05 -0400

    Update run_script_dcbf.sh

--- my_scripts/run_script_dcbf.sh
+++ my_scripts/run_script_dcbf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 for test_name in dcbf_1
 do
-  for gamma in 0.99
+  for gamma in 0.5 0.9 0.99
   do
-    for time_steps in 2
+    for time_steps in 2 4 6
     do
       ./gpu_batch_dcbf.sh $test_name $gamma $time_steps
     done
