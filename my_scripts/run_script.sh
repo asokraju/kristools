6d2bf71d8f65ff673bda855d483bcf4352a401b8
asokraju <kkrishnachaitanya89@gmail.com>
2020-08-24 23:20:24 -0400

    Update run_script.sh

--- my_scripts/run_script.sh
+++ my_scripts/run_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 for test_name in 1
 do
-  for gamma in 0.6 0.9 0.99
+  for gamma in 0.99
   do
-    for time_steps in 2 4 6 8
+    for time_steps in 2
     do
       ./gpu_batch $gamma $time_steps
     done
