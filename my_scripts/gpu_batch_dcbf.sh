773bddbdceaf740f4183e5d1d600dc884b0963ea
asokraju <kkrishnachaitanya89@gmail.com>
2020-08-25 01:35:10 -0400

    Added comment to .sh files

--- my_scripts/gpu_batch_dcbf.sh
+++ my_scripts/gpu_batch_dcbf.sh
@@ -4,7 +4,7 @@ TEST_NAME=$1
 GAMMA=$2
 TS=$3
 
-
+#Creating necessary folders to save the results of the experiment
 PARENT_DIR="$(dirname $PWD)"             #file is inside this  directory
 EXEC_DIR=$PWD                            #gpu_batch script is inside this dir
 TEST_NAME_DIR="test_name=${TEST_NAME}"   #directory with test name
@@ -20,17 +20,17 @@ cd $GAMMA_DIR
 mkdir -p $TS_DIR                         #making a directory for parameter time steps name
 cd $TS_DIR
 
-export run_exec=$PARENT_DIR/microgrid_dcbf.py
+export run_exec=$PARENT_DIR/microgrid_dcbf.py #python script that we want to run
 #export run_exec=/afs/crc.nd.edu/user/k/kkosaraj/kristools/microgrid_dcbf.py
-export run_flags="--gamma=${GAMMA} --time_steps=${TS} --summary_dir='$PWD/' > out.txt"  
+export run_flags="--gamma=${GAMMA} --time_steps=${TS} --summary_dir='$PWD/' > out.txt"   #flags for the script
 
-echo "#!/bin/bash" > job.sh
-echo "#$ -q gpu" >> job.sh
-echo "#$ -l gpu_card=1" >>job.sh
+echo "#!/bin/bash" > job.sh                         
+echo "#$ -q gpu" >> job.sh                           # which queue to use: debug, long, gpu
+echo "#$ -l gpu_card=1" >>job.sh                       # need if we use gpu queue
 #echo "#$ -pe smp 1" >> job.sh
-echo "#$ -N DCBF_gamma=${GAMMA}_TS=${TS}" >> job.sh
+echo "#$ -N DCBF_gamma=${GAMMA}_TS=${TS}" >> job.sh   # name for job
 echo "#$ -o info" >> job.sh
-echo "module load conda" >> job.sh
+echo "module load conda" >> job.sh                   #loading the desired modules
 echo "module load cuda" >> job.sh
 echo "module load cudnn" >> job.sh
 echo "conda activate tf_gpu_krishna" >> job.sh
